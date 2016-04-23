2ad82420f18e84b3108b3d033877b9cd08fd9cbd
ongunsavas <osavas@purdue.edu>
2016-04-23 18:22:11 -0400

    helooo
    🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
-    require "sinatra"
-
-    get "/" do
-        "Hello, world!"
-    end
+require 'sinatra'
+get "/" do
+	"Hello, world!"
+end
+get "restaurants" do
+	chicago = ["Purple Pig", "Boho House", "Gibsons"]
+	new_york = ["Blue Ribbon", "Fogo", "Sipsak"]
+	case params[:id]
+	when 1
+		chicago.sample
+	when 2
+		new_york.sample
+	else
+		"The city is not found in our database"
+		end
+end
